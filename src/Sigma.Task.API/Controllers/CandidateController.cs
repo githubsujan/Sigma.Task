47113e79136e608992341b319ea87cf9052ea6c9
Sujan.Karki <Sujan.Karki@CGNPLAP924>
2024-11-25 11:14:09 +0545

    feature: added candidate controller in API project

---
+++ src/Sigma.Task.API/Controllers/CandidateController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Sigma.Task.Application.Commands;
+using Sigma.Task.Application.DTOs;
+using Sigma.Task.Application.Queries;
+
+namespace Sigma.Task.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CandidateController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public CandidateController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet("GetCandidates")]
+        public async Task<IActionResult> GetCandidatesAsync()
+        {
+            var result = await _sender.Send(new GetCandidatesQuery());
+            return Ok(result);
+        }
+
+        [HttpPost("SaveCandidate")]
+        public async Task<IActionResult> SaveCandidate([FromBody] CandidateDTO candidateDTO)
+        {
+            var result = await _sender.Send(new SaveCandidateCommand(candidateDTO));
+            return Ok(result);
+        }
+    }
+}
