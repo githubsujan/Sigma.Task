8a9cc1e2b8d9bc50037afd4c6a56a989021a54cd
Sujan.Karki <Sujan.Karki@CGNPLAP924>
2024-11-25 11:17:14 +0545

    feature: initial migration added in Infrastructure project

---
+++ src/Sigma.Task.Infrastructure/Migrations/20241125053034_AddTableCandidate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sigma.Task.Infrastructure.Migrations
+{
+    public partial class AddTableCandidate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    CallSchedule = table.Column<DateTime>(type: "datetime", nullable: true),
+                    LinkedInProfile = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    GitHubProfile = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Comment = table.Column<string>(type: "nvarchar(500)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    LastUpdatedDate = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
